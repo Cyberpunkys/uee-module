f06f2ef016929b472b30b645db38d95a9bb91eb0
cyberpunkys <h4k3r88@gmail.com>
2022-06-13 16:48:26 +0300

    add spring security and simple login

--- src/main/java/ru/vstu/ueemodule/controller/MainController.java
+++ src/main/java/ru/vstu/ueemodule/controller/MainController.java
@@ -5,15 +5,18 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @PropertySource("classpath:main.properties")
+@RequestMapping("/")
 public class MainController {
 
     @Value("${university.name}")
     private String universityName;
 
-    @GetMapping("/")
+    @GetMapping
     public String hello(Model model) {
         model.addAttribute("name", universityName);
 
